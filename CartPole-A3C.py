b5a3fc3e3b8d839657b3a8dc6b9dd1f5a8748e7b
haiyinpiao <haiyinpiao@qq.com>
2018-03-23 10:45:25 +0800

    try to add batch_predict and g_predict_lock feature to overcoming batch prediction issue

--- CartPole-A3C.py
+++ CartPole-A3C.py
@@ -15,8 +15,7 @@ import gym, time, random
 
 # import threading
 
-import multiprocessing
-from multiprocessing import Process, Lock
+import multiprocessing as mp
 
 from keras.models import *
 from keras.layers import *
@@ -40,7 +39,6 @@ ENV = 'CartPole-v0'
 
 RUN_TIME = 30
 THREADS = 4
-OPTIMIZERS = 2
 THREAD_DELAY = 0.001
 
 GAMMA = 0.99
@@ -58,11 +56,18 @@ LEARNING_RATE = 5e-3
 LOSS_V = .5			# v loss coefficient
 LOSS_ENTROPY = .01 	# entropy coefficient
 
+# multiprocess global sample queue for batch traning.
+g_train_queue = [  mp.Array('f') for i in range(5) ]	# s, a, r, s', s' terminal mask
+g_train_lock = mp.Lock()
+
+# multiprocess global state queue for action predict
+g_predict_queue = [ mp.Array('f') ]
+g_predict_lock = mp.Lock()
+
 #---------
 class Brain:
-	train_queue = [  mp.Queue() for i in range(5) ]	# s, a, r, s', s' terminal mask
-	# lock_queue = threading.Lock()
-	lock_queue = Lock()
+	# train_queue = [  mp.Queue() for i in range(5) ]	# s, a, r, s', s' terminal mask
+	# lock_queue = Lock()
 
 	def __init__(self):
 		
@@ -113,17 +118,17 @@ class Brain:
 
 		return s_t, a_t, r_t, minimize
 
-	def optimize(self):
-		if len(self.train_queue[0]) < MIN_BATCH:
-			time.sleep(0)	# yield
-			return
+	def batch_optimize(self):
+		with g_train_lock:
+			if len(g_train_queue[0]) < MIN_BATCH:
+				time.sleep(0)	# yield
+				return
 
-		with self.lock_queue:
-			if len(self.train_queue[0]) < MIN_BATCH:	# more thread could have passed without lock
+			if len(g_train_queue[0]) < MIN_BATCH:	# more thread could have passed without lock
 				return 									# we can't yield inside lock
 
-			s, a, r, s_, s_mask = self.train_queue
-			self.train_queue = [ [], [], [], [], [] ]
+			s, a, r, s_, s_mask = g_train_queue
+			g_train_queue = [ [], [], [], [], [] ]
 
 		s = np.vstack(s)
 		a = np.vstack(a)
@@ -131,7 +136,7 @@ class Brain:
 		s_ = np.vstack(s_)
 		s_mask = np.vstack(s_mask)
 
-		if len(s) > 5*MIN_BATCH: print("Optimizer alert! Minimizing batch of %d" % len(s))
+		if len(s) > 5*MIN_BATCH: print("Optimizer alert! Minimizing train batch of %d" % len(s))
 
 		v = self.predict_v(s_)
 		r = r + GAMMA_N * v * s_mask	# set v to 0 where s_ is terminal state
@@ -139,22 +144,46 @@ class Brain:
 		s_t, a_t, r_t, minimize = self.graph
 		self.session.run(minimize, feed_dict={s_t: s, a_t: a, r_t: r})
 
+	def batch_predict(self):
+		with g_predict_lock:
+			if len(g_predict_queue[0]) < MIN_BATCH:
+				time.sleep(0)	# yield
+				return	
+
+			if len(g_predict_queue[0]) < MIN_BATCH:	# more thread could have passed without lock
+				return 									# we can't yield inside lock
+		
+			s = g_predict_queue
+			s = np.vstack(s)
+			g_train_queue = []
+
+		if len(s) > 5*MIN_BATCH: 
+			print("Optimizer alert! Minimizing predict batch of %d" % len(s))
+		
+		p = self.predict_p(s)[0]
+
+		return p
+
 	def train_push(self, s, a, r, s_):
-		with self.lock_queue:
-			self.train_queue[0].append(s)
-			self.train_queue[1].append(a)
-			self.train_queue[2].append(r)
+		with g_train_lock:
+			g_train_queue[0].append(s)
+			g_train_queue[1].append(a)
+			g_train_queue[2].append(r)
 
 			if s_ is None:
-				self.train_queue[3].append(NONE_STATE)
-				self.train_queue[4].append(0.)
+				g_train_queue[3].append(NONE_STATE)
+				g_train_queue[4].append(0.)
 			else:	
-				self.train_queue[3].append(s_)
-				self.train_queue[4].append(1.)
+				g_train_queue[3].append(s_)
+				g_train_queue[4].append(1.)
+
+	def predict_push(self, s, i):
+		with g_pridict_lock:
+			g_predict_queue.append(s)
 
 	def predict(self, s):
 		with self.default_graph.as_default():
-			p, v = self.model.predict(s)
+			p, v = self.model.predict(s)		
 			return p, v
 
 	def predict_p(self, s):
@@ -193,7 +222,8 @@ class Agent:
 
 		else:
 			s = np.array([s])
-			p = brain.predict_p(s)[0]
+			brain.predict_push( s )
+			# p = brain.predict_p(s)[0]
 
 			# a = np.argmax(p)
 			a = np.random.choice(NUM_ACTIONS, p=p)
@@ -237,7 +267,7 @@ class Agent:
 #---------
 # class Environment(threading.Thread):
 class Environment(Process):
-	stop_signal = False
+	# stop_signal = False
 
 	def __init__(self, render=False, eps_start=EPS_START, eps_end=EPS_STOP, eps_steps=EPS_STEPS):
 		# threading.Thread.__init__(self)
@@ -267,7 +297,7 @@ class Environment(Process):
 			s = s_
 			R += r
 
-			if done or self.stop_signal:
+			if done #or self.stop_signal:
 				break
 
 		log_reward( R )
@@ -277,24 +307,25 @@ class Environment(Process):
 		while not self.stop_signal:
 			self.runEpisode()
 
-	def stop(self):
-		self.stop_signal = True
+	# def stop(self):
+	# 	self.stop_signal = True
 
 #---------
 # class Optimizer(threading.Thread):
-class Optimizer(Process):
-	stop_signal = False
+class Optimizer:
+	# stop_signal = False
 
-	def __init__(self):
-		# threading.Thread.__init__(self)
-		Process.__init__(self)
+	# def __init__(self):
+	# 	# threading.Thread.__init__(self)
+	# 	mp.Process.__init__(self)
 
 	def run(self):
-		while not self.stop_signal:
-			brain.optimize()
+		while time.time()-start<RUN_TIME:
+			brain.batch_predict()
+			brain.batch_optimize()
 
-	def stop(self):
-		self.stop_signal = True
+	# def stop(self):
+	# 	self.stop_signal = True
 
 #-- main
 env_test = Environment(render=True, eps_start=0., eps_end=0.)
@@ -305,25 +336,21 @@ NONE_STATE = np.zeros(NUM_STATE)
 brain = Brain()	# brain is global in A3C
 
 envs = [Environment() for i in range(THREADS)]
-opts = [Optimizer() for i in range(OPTIMIZERS)]
-
-for o in opts:
-	o.start()
+opts = Optimizer()
 
 for e in envs:
 	e.start()
 
-time.sleep(RUN_TIME)
+opts.run()
+
+# time.sleep(RUN_TIME)
 
 for e in envs:
 	e.stop()
 for e in envs:
 	e.join()
 
-for o in opts:
-	o.stop()
-for o in opts:
-	o.join()
+# opts.stop()
 
 print("Training finished")
 
